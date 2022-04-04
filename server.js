b4d2fab0e19b03ab74785ce5668a2c172142ba17
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-04-04 14:19:42 +0545

    minor changes

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.route('/')
 // Respond not found to all the wrong routes
 app.use(function (req, res, next) {
   res.status(404);
-  res.type('txt').send('Not found lol');
+  res.type('txt').send('Not Found.');
 });
 
 // Error Middleware
